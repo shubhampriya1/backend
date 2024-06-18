570d9b022f3d169e95438d3b992a9091e033f7b8
Shubham priya <141516634+shubhampriya1@users.noreply.github.com>
2024-06-18 19:59:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-// require("dotenv").config();
-
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
 app.get("/twitter", (req, res) => {
-  res.send("sab log pagal hai shubhsm bs achi h");
+  res.send("sab log acche hai shubham bs pagal h");
 });
+
 app.listen(process.env.PORT, () => {
   console.log(`exmple app listenting on port ${port}`);
 });
